7e6d2da5934e6183a144424f8f38165037206180
cactusjack27 <77975999+cactusjack27@users.noreply.github.com>
2025-02-20 14:14:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(){
     function updateCountdown(){
     let now = new Date();
-    let month = 5;
+    let months = now.getMonth()+12+4;
     let targetDay = 21;
+    let alertShown = false;
     let targetTime = new Date(now.getFullYear(), now.getMonth(), targetDay, 0, 0, 0, 0);
-    if(now >= targetTime) {
-        targetTime = new Date(now.getFullYear(), now.getMonth()+1, targetDay, 0, 0, 0, 0);
-        month = month+1;
+    if (now >= targetTime) {
+        targetTime = new Date(now.getFullYear(), now.getMonth() + 1, targetDay, 0, 0, 0, 0);
+        if(now.getDate() > targetDay){
+            months = months+1;
+        }
     }
     let timeleft = targetTime-now;
     let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
@@ -17,9 +20,14 @@ document.addEventListener("DOMContentLoaded", function(){
     hours = hours.toString().padStart(2, "0");
     minutes = minutes.toString().padStart(2, "0");
     seconds = seconds.toString().padStart(2, "0");
-
+    let years = parseInt(months/12);
+    months = months%12;
     document.getElementById("timer").textContent = `${days}d, ${hours}h, ${minutes}m, ${seconds}s`;
-    document.getElementById("updtext").textContent = `1 year, ${month} months`
+    document.getElementById("updtext").textContent = `${years} year, ${months} months`;
+    if (timeLeft <= 0 && !sessionStorage.getItem("alertShown")) {
+        alert("love youuu <3");
+        sessionStorage.setItem("alertShown", "true");
+    }
     }
     setInterval(updateCountdown, 1000);
     updateCountdown();
