163bcd9c531a6dbf5859701d564bf1965075fb09
cactusjack27 <77975999+cactusjack27@users.noreply.github.com>
2025-02-21 16:11:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -24,12 +24,29 @@ document.addEventListener("DOMContentLoaded", function(){
     months = months%12;
     document.getElementById("timer").textContent = `${days}d, ${hours}h, ${minutes}m, ${seconds}s`;
     document.getElementById("updtext").textContent = `${years} year, ${months} months`;
-    if (timeLeft <= 0 && !sessionStorage.getItem("alertShown")) {
+    if (timeLeft <= 0 && sessionStorage.getItem("alertShown") == "false") {
         alert("love youuu <3");
         sessionStorage.setItem("alertShown", "true");
     }
     }
+    let slides = document.querySelectorAll(".slide");
+    let currentIndex = 0;
+    function showSlide(index){
+        slides.forEach(function(slide, i){
+            slide.classList.remove("show");
+            if(i==index){
+                slide.classList.add("show");
+            }
+        });
+    }
+    function nextSlide(){
+        currentIndex = (currentIndex+1)%slides.length;
+        showSlide(currentIndex);
+    }
+    showSlide(currentIndex);
+    setInterval(nextSlide,3000);
     setInterval(updateCountdown, 1000);
     updateCountdown();
+
 
 });
